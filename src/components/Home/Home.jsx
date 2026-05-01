8665c425c8cb3d1a067ede3e1aaa70214a0ce0e4
German Milla <pymosv@gmail.com>
2026-04-30 22:46:17 -0600

    small changes in appbar

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,6 +5,7 @@ import { useSelector, useDispatch } from 'react-redux'
 import heroImg from '../../assets/hero.png'
 import reactLogo from '../../assets/react.svg'
 import viteLogo from '../../assets/vite.svg'
+import Navbar from '../Navbar/Navbar'
 
 function Home() {
     const { user } = useSelector((state) => state.auth)
@@ -16,19 +17,26 @@ function Home() {
     }
 
     return (
+    
+    
+    <>
+        <Navbar user={user} />
+        <section id="center">
+            <div className="hero">
+                <img src={heroImg} className="base" width="170" height="179" alt="" />
+                <img src={reactLogo} className="framework" alt="React logo" />
+                <img src={viteLogo} className="vite" alt="Vite logo" />
+            </div>
+            <div>
+                <h1>Welcome, {user?.displayName || 'User'}!</h1>
+                <h2 style={{ color: '#d97757' }}>You are logged in</h2>
+                <button className="counter" onClick={handleLogout}>Sign Out</button>
+            </div>
+        </section>
+    
+    </>
 
-    <section id="center">
-        <div className="hero">
-            <img src={heroImg} className="base" width="170" height="179" alt="" />
-            <img src={reactLogo} className="framework" alt="React logo" />
-            <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-            <h1>Welcome, {user?.displayName || 'User'}!</h1>
-            <h2 style={{ color: '#d97757' }}>You are logged in</h2>
-            <button className="counter" onClick={handleLogout}>Sign Out</button>
-        </div>
-    </section>
+
 )}
 
 export default Home
